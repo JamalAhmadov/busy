00a9054f95b3c5a53ecbc89740e74ac4ec43d18c
Jamal Ahmadov <camalehmedov12@gmail.com>
2024-06-27 21:38:48 +0400

    ada

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Profile from "./Pages/Userpages/Profile";
 import Settings from "./Pages/Userpages/Settings";
 import Bookmarks from "./Pages/Userpages/Bookmarks";
 import Information from "./Pages/Userpages/Information";
+import EProfile from "./Pages/Employerpages/Profile";
+import ESettings from "./Pages/Employerpages/Settings";
+import Emlposts from "./Pages/Employerpages/Emlposts";
+import EInformation from "./Pages/Employerpages/Information";
+import EPost from "./Pages/Employerpages/EmployerPost";
 
 const App = () => {
   return (
@@ -19,10 +24,16 @@ const App = () => {
 
         {/* Yeni */}
 
-        <Route path="/dashboard/profile" element={<Profile />}>
+        <Route path="/dashboard/jobseeker/profile" element={<Profile />}>
           <Route path="" element={<Information />} />
           <Route path="bookmarks" element={<Bookmarks />} />
           <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="/dashboard/employer/profile" element={<EProfile />}>
+          <Route path="" element={<EInformation />} />
+          <Route path="post" element={<EPost />} />
+          <Route path="emlposts" element={<Emlposts />} />
+          <Route path="settings" element={<ESettings />} />
         </Route>
       </Routes>
       <Footer />
